51ec11b1522a88958e82a6d346e01ce126c6cb3a
Marcel Guzik <mguzik555@gmail.com>
2019-01-19 19:27:54 +0100

    Code for enabling pins

--- app.py
+++ app.py
@@ -1,14 +1,19 @@
-from socket import *
+from socket import socket, AF_INET, SOCK_DGRAM
+from gpiozero import LED
 
 PORT = 12345  # arbitrary, just make it match in Android code
 IP = "0.0.0.0"  # represents any IP address
 
+led_pins = [2, 3, 4]
+leds = [LED(i) for i in led_pins]
+
 sock = socket(AF_INET, SOCK_DGRAM)  # SOCK_DGRAM means UDP socket
-sock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)  # make socket reuseable
 sock.bind((IP, PORT))
 
 while True:
     print("Waiting for data...")
-    data, addr = sock.recvfrom(1024)  # blocking
-    print("received: ")
-    print(data)
+    data, addr = sock.recvfrom(2)  # blocking
+    pin, state = data
+    print('received: ')
+    print(data, state)
+    leds[pin].toggle()
