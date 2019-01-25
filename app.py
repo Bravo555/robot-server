03e16816334e03b50f616e98ba5e230ea171b7da
Marcel Guzik <mguzik555@gmail.com>
2019-01-25 16:22:14 +0100

    Testing stuff

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 from socket import socket, AF_INET, SOCK_DGRAM
 from gpiozero import LED
+from PCA9685 import PCA9685
 
 PORT = 12345  # arbitrary, just make it match in Android code
 IP = "0.0.0.0"  # represents any IP address
 
 led_pins = [2, 17, 10]
 leds = [LED(i) for i in led_pins]
+servo = PCA9685(0x40, debug=False)
+servo.setPWMFreq(50)
 
 sock = socket(AF_INET, SOCK_DGRAM)  # SOCK_DGRAM means UDP socket
 sock.bind((IP, PORT))
@@ -13,10 +16,15 @@ sock.bind((IP, PORT))
 while True:
     print("Waiting for data...")
     data, addr = sock.recvfrom(2)  # blocking
-    pin, state = data
+    address, state = data
+
     print('received: ')
     print(data)
-    if state == 0x0:
-        leds[pin].off()
-    else:
-        leds[pin].on()
+    # address: 1-3 LEDs, 255 is servo
+    if address in range(0x0, 0x02):
+        if state == 0x0:
+            leds[address].off()
+        else:
+            leds[address].on()
+    elif address == 0xff:
+        pass
